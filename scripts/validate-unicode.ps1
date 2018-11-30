2eda0b97f09d2505eabffc488734b2aae7797177
Mariia Mykhailova <mamykhai@microsoft.com>
2018-11-29 23:41:44 -0800

    Try error messaging with color highlight

--- scripts/validate-unicode.ps1
+++ scripts/validate-unicode.ps1
@@ -4,6 +4,6 @@ $lines
 
 # If there are any, print them and exit with an error
 if ($lines.Count -gt 0) {
-    Write-Error "Please use U+27E9 for ket symbol and U+27E8 for bra symbol"
+    Write-Host "$("##vso[task.logissue type=error;]") $("Please use U+27E9 for ket symbol and U+27E8 for bra symbol")"
     exit 1
 }
