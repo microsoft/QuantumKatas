db833852b94d387be59629aef4082de1f72e9262
Mariia Mykhailova <mamykhai@microsoft.com>
2018-11-29 23:16:53 -0800

    Add a check for using proper bra and ket symbols

---
+++ scripts/validate-unicode.ps1
@@ -0,0 +1,9 @@
+﻿# Find all lines with bad bra or ket characters
+$lines = Select-String -Path "**\*.qs" -Pattern "[〉〈]"
+$lines
+
+# If there are any, print them and exit with an error
+if ($lines.Count > 0) {
+    Write-Error "Please use U+27E9 for ket symbol and U+27E8 for bra symbol"
+    exit 1
+}
