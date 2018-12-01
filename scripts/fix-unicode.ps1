2344fd48cd4169ab203f7490f5938a7e721fda5f
Mariia Mykhailova <mamykhai@microsoft.com>
2018-12-01 13:26:47 -0800

    Add a script to replace symbols automatically

---
+++ scripts/fix-unicode.ps1
@@ -0,0 +1,9 @@
+﻿$files = Select-String -Path "**\*.qs" -pattern "[〉〈]" | Select-Object -Unique Path
+
+$files | ForEach-Object {
+    # Get-Content by default reads file in UTF8 encoding
+    $Content = (Get-Content -Encoding UTF8 $_.Path) | ForEach-Object { $_ -Replace "〉", "⟩" } | ForEach-Object { $_ -replace "〈", "⟨" }
+    # Make sure the file is written without BOM
+    $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+    [System.IO.File]::WriteAllLines($_.Path, $Content, $Utf8NoBomEncoding)
+}
