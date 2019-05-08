microsoft/QuantumKatas

commit 7519eb3f74a82066e4fc1cecc81531cd3bd4d476

Updating Katas to QDK 0.6 (#111)
* Update the katas to use QDK 0.6.1905.301.
* Add mechanism to verify that the reference solutions actually solve all tasks of a Notebook correctly.

Co-Authored-By: Bettina Heim <34236215+bettinaheim@users.noreply.github.com>
Co-Authored-By: Andres Paz <anpaz@microsoft.com>

PhaseEstimation/ReferenceImplementation.qs
@@ -10,11 +10,15 @@
 
 namespace Quantum.Kata.PhaseEstimation {
     
-    open Microsoft.Quantum.Extensions.Convert;
-    open Microsoft.Quantum.Extensions.Math;
-    open Microsoft.Quantum.Extensions.Testing;
-    open Microsoft.Quantum.Primitive;
+    open Microsoft.Quantum.Measurement;
+    open Microsoft.Quantum.Characterization;
+    open Microsoft.Quantum.Arithmetic;
+    open Microsoft.Quantum.Oracles;
+    open Microsoft.Quantum.Intrinsic;
     open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Diagnostics;
+    open Microsoft.Quantum.Convert;
+    open Microsoft.Quantum.Math;
     
     
     //////////////////////////////////////////////////////////////////
@@ -22,39 +26,32 @@ namespace Quantum.Kata.PhaseEstimation {
     //////////////////////////////////////////////////////////////////
     
     // Task 1.1. Inputs to QPE: eigenstates of Z/S/T gates.
-    operation Eigenstates_ZST_Reference (q : Qubit, state : Int) : Unit {
+    operation Eigenstates_ZST_Reference (q : Qubit, state : Int) : Unit
+    is Adj {
         
-        body (...) {
-            if (state == 1) {
-                X(q);
-            }
+        if (state == 1) {
+            X(q);
         }
-        
-        adjoint auto;
     }
 
 
     // ------------------------------------------------------
-    operation UnitaryPowerImpl_Reference (U : (Qubit => Unit : Adjoint, Controlled), power : Int, q : Qubit) : Unit {
-        body (...) {
-            for (i in 1..power) {
-                U(q);
-            }
+    operation UnitaryPowerImpl_Reference (U : (Qubit => Unit is Adj + Ctl), power : Int, q : Qubit) : Unit
+    is Adj + Ctl {
+        for (i in 1..power) {
+            U(q);
         }
-        adjoint auto;
-        controlled auto;
-        controlled adjoint auto;
     }
 
     // Task 1.2. Inputs to QPE: powers of Z/S/T gates.
-    function UnitaryPower_Reference (U : (Qubit => Unit : Adjoint, Controlled), power : Int) : (Qubit => Unit : Adjoint, Controlled) {
+    function UnitaryPower_Reference (U : (Qubit => Unit is Adj + Ctl), power : Int) : (Qubit => Unit is Adj + Ctl) {
         return UnitaryPowerImpl_Reference(U, power, _);
     }
 
 
     // ------------------------------------------------------
     // Task 1.3. Validate inputs to QPE
-    operation AssertIsEigenstate_Reference (U : (Qubit => Unit), P : (Qubit => Unit : Adjoint)) : Unit {
+    operation AssertIsEigenstate_Reference (U : (Qubit => Unit), P : (Qubit => Unit is Adj)) : Unit {
         using (q = Qubit()) {
             // Prepare the state |ψ⟩
             P(q);
@@ -69,22 +66,17 @@ namespace Quantum.Kata.PhaseEstimation {
 
 
     // ------------------------------------------------------
-    operation Oracle_Reference (U : (Qubit => Unit : Adjoint, Controlled), power : Int, target : Qubit[]) : Unit {
-        body (...) {
-            for (i in 1 .. power) {
-                U(target[0]);
-            }
+    operation Oracle_Reference (U : (Qubit => Unit is Adj + Ctl), power : Int, target : Qubit[]) : Unit 
+    is Adj + Ctl{
+        for (i in 1 .. power) {
+            U(target[0]);
         }
-        adjoint auto;
-        controlled auto;
-        controlled adjoint auto;
     }
 
     // Task 1.4. QPE for single-qubit unitaries
-    operation QPE_Reference (U : (Qubit => Unit : Adjoint, Controlled), P : (Qubit => Unit : Adjoint), n : Int) : Double {
+    operation QPE_Reference (U : (Qubit => Unit is Adj + Ctl), P : (Qubit => Unit is Adj), n : Int) : Double {
         // Construct a phase estimation oracle from the unitary
         let oracle = DiscreteOracle(Oracle_Reference(U, _, _));
-        mutable phase = -1.0;
         // Allocate qubits to hold the eigenstate of U and the phase in a big endian register 
         using ((eigenstate, phaseRegister) = (Qubit[1], Qubit[n])) {
             let phaseRegisterBE = BigEndian(phaseRegister);
@@ -93,13 +85,12 @@ namespace Quantum.Kata.PhaseEstimation {
             // Call library
             QuantumPhaseEstimation(oracle, eigenstate, phaseRegisterBE);
             // Read out the phase
-            set phase = ToDouble(MeasureIntegerBE(phaseRegisterBE)) / ToDouble(1 <<< n);
+            let phase = IntAsDouble(MeasureIntegerBE(phaseRegisterBE)) / IntAsDouble(1 <<< n);
 
             ResetAll(eigenstate);
             ResetAll(phaseRegister);
+            return phase;
         }
-
-        return phase;
     }
 
 
@@ -108,80 +99,70 @@ namespace Quantum.Kata.PhaseEstimation {
     //////////////////////////////////////////////////////////////////
     
     // Task 2.1. Single-bit phase estimation
-    operation SingleBitPE_Reference (U : (Qubit => Unit : Adjoint, Controlled), P : (Qubit => Unit : Adjoint)) : Int {
-        mutable eigenvalue = 0;
+    operation SingleBitPE_Reference (U : (Qubit => Unit is Adj + Ctl), P : (Qubit => Unit is Adj)) : Int {
         using ((control, eigenstate) = (Qubit(), Qubit())) {
             // prepare the eigenstate |ψ⟩
             P(eigenstate);
 
             H(control);
-            (Controlled U)([control], eigenstate);
+            Controlled U([control], eigenstate);
             H(control);
 
-            set eigenvalue = M(control) == Zero ? 1 | -1;
-
+            let eigenvalue = M(control) == Zero ? 1 | -1;
             ResetAll([control, eigenstate]);
+            return eigenvalue;
         }
-        return eigenvalue;
     }
 
 
     // Task 2.2. Two bit phase estimation
-    operation TwoBitPE_Reference (U : (Qubit => Unit : Adjoint, Controlled), P : (Qubit => Unit : Adjoint)) : Double {
+    operation TwoBitPE_Reference (U : (Qubit => Unit is Adj + Ctl), P : (Qubit => Unit is Adj)) : Double {
+
         // Start by using the same circuit as in task 2.1.
         // For eigenvalues +1 and -1, it produces measurement results Zero and One, respectively, 100% of the time;
         // for eigenvalues +i and -i, it produces both results with 50% probability, so a different circuit is required.
-        mutable (nZero, nOne) = (0, 0);
         using ((control, eigenstate) = (Qubit(), Qubit())) {
             // prepare the eigenstate |ψ⟩
             P(eigenstate);
 
+            mutable (measuredZero, measuredOne) = (false, false); 
             mutable iter = 0;
             repeat {
-                set iter = iter + 1;
+                set iter += 1;
 
                 H(control);
-                (Controlled U)([control], eigenstate);
+                Controlled U([control], eigenstate);
                 H(control);
 
-                if (MResetZ(control) == Zero) {
-                    set nZero = nZero + 1;
-                } else {
-                    set nOne = nOne + 1;
-                }
-
-                // repeat the loop until we get both Zero and One measurement outcomes
-                // or until we're reasonably certain that we won't get a different outcome
-            } until (iter == 10 or nZero > 0 and nOne > 0)
+                let meas = MResetZ(control);
+                set (measuredZero, measuredOne) = (meas == Zero, meas == One);
+            } 
+            // repeat the loop until we get both Zero and One measurement outcomes
+            // or until we're reasonably certain that we won't get a different outcome
+            until (iter == 10 or measuredZero and measuredOne)
             fixup {}
-
             Reset(eigenstate);
-        }
 
-        if (nOne == 0) {
             // all measurements yielded Zero => eigenvalue +1
-            return 0.0;
-        }
-        if (nZero == 0) {
             // all measurements yielded One => eigenvalue -1
-            return 0.5;
+            if (not measuredZero or not measuredOne) {
+                return measuredOne ? 0.5 | 0.0;
+            }
         }
 
         // To distinguish between eigenvalues i and -i, we need a circuit with an extra S gate on control qubit
-        mutable eigenvalue = -1.0;
         using ((control, eigenstate) = (Qubit(), Qubit())) {
             // prepare the eigenstate |ψ⟩
             P(eigenstate);
 
             H(control);
-            (Controlled U)([control], eigenstate);
+            Controlled U([control], eigenstate);
             S(control);
             H(control);
 
-            set eigenvalue = MResetZ(control) == Zero ? 0.75 | 0.25;
-
+            let eigenvalue = MResetZ(control) == Zero ? 0.75 | 0.25;
             Reset(eigenstate);
+            return eigenvalue;
         }
-        return eigenvalue;
     }
 }

BasicGates/ReferenceImplementation.qs
@@ -10,7 +10,7 @@
 
 namespace Quantum.Kata.BasicGates {
     
-    open Microsoft.Quantum.Primitive;
+    open Microsoft.Quantum.Intrinsic;
     open Microsoft.Quantum.Canon;
     
     
@@ -72,27 +72,19 @@ namespace Quantum.Kata.BasicGates {
     //        If the qubit is in state |0⟩, change its state to cos(alpha)*|0⟩ + sin(alpha)*|1⟩.
     //        If the qubit is in state |1⟩, change its state to -sin(alpha)*|0⟩ + cos(alpha)*|1⟩.
     //        If the qubit is in superposition, change its state according to the effect on basis vectors.
-    operation AmplitudeChange_Reference (q : Qubit, alpha : Double) : Unit {
-        
-        body (...) {
-            Ry(2.0 * alpha, q);
-        }
-        
-        adjoint invert;
+    operation AmplitudeChange_Reference (q : Qubit, alpha : Double) : Unit
+    is Adj {        
+        Ry(2.0 * alpha, q);
     }
     
     
     // Task 1.5. Phase flip
     // Input: A qubit in state |ψ⟩ = α |0⟩ + β |1⟩.
     // Goal:  Change the qubit state to α |0⟩ + iβ |1⟩ (flip the phase of |1⟩ component of the superposition).
-    operation PhaseFlip_Reference (q : Qubit) : Unit {
-        
-        body (...) {
-            S(q);
-            // alternatively Rz(0.5 * PI(), q);
-        }
-        
-        adjoint invert;
+    operation PhaseFlip_Reference (q : Qubit) : Unit 
+    is Adj {        
+        S(q);
+        // alternatively Rz(0.5 * PI(), q);
     }
     
     
@@ -104,54 +96,38 @@ namespace Quantum.Kata.BasicGates {
     //        If the qubit is in state |0⟩, don't change its state.
     //        If the qubit is in state |1⟩, change its state to exp(i*alpha)|1⟩.
     //        If the qubit is in superposition, change its state according to the effect on basis vectors.
-    operation PhaseChange_Reference (q : Qubit, alpha : Double) : Unit {
-        
-        body (...) {
-            Rz(alpha, q);
-        }
-        
-        adjoint invert;
+    operation PhaseChange_Reference (q : Qubit, alpha : Double) : Unit
+    is Adj {        
+        Rz(alpha, q);
     }
     
     
     // Task 1.7. Bell state change - 1
     // Input: Two entangled qubits in Bell state |Φ⁺⟩ = (|00⟩ + |11⟩) / sqrt(2).
     // Goal:  Change the two-qubit state to |Φ⁻⟩ = (|00⟩ - |11⟩) / sqrt(2).
-    operation BellStateChange1_Reference (qs : Qubit[]) : Unit {
-        
-        body (...) {
-            Z(qs[0]);
-            // alternatively Z(qs[1]);
-        }
-        
-        adjoint invert;
+    operation BellStateChange1_Reference (qs : Qubit[]) : Unit
+    is Adj {        
+        Z(qs[0]);
+        // alternatively Z(qs[1]);
     }
     
     
     // Task 1.8. Bell state change - 2
     // Input: Two entangled qubits in Bell state |Φ⁺⟩ = (|00⟩ + |11⟩) / sqrt(2).
     // Goal:  Change the two-qubit state to |Ψ⁺⟩ = (|01⟩ + |10⟩) / sqrt(2).
-    operation BellStateChange2_Reference (qs : Qubit[]) : Unit {
-        
-        body (...) {
-            X(qs[0]);
-            // alternatively X(qs[1]);
-        }
-        
-        adjoint invert;
+    operation BellStateChange2_Reference (qs : Qubit[]) : Unit
+    is Adj {        
+        X(qs[0]);
+        // alternatively X(qs[1]);
     }
     
     
     // Task 1.9. Bell state change - 3
     // Input: Two entangled qubits in Bell state |Φ⁺⟩ = (|00⟩ + |11⟩) / sqrt(2).
     // Goal:  Change the two-qubit state to |Ψ⁻⟩ = (|01⟩ - |10⟩) / sqrt(2).
-    operation BellStateChange3_Reference (qs : Qubit[]) : Unit {
-        
-        body (...) {
-            Y(qs[0]);
-        }
-        
-        adjoint invert;
+    operation BellStateChange3_Reference (qs : Qubit[]) : Unit
+    is Adj {        
+        Y(qs[0]);
     }
     
     

CHSHGame/ReferenceImplementation.qs
@@ -43,35 +43,26 @@ namespace Quantum.Kata.CHSHGame {
     //////////////////////////////////////////////////////////////////
 
     // Task 2.1. Entangled pair
-    operation CreateEntangledPair_Reference (qs : Qubit[]) : Unit {
-        body (...) {
-            H(qs[0]);
-            CNOT(qs[0], qs[1]);
-        }
-
-        adjoint invert;
+    operation CreateEntangledPair_Reference (qs : Qubit[]) : Unit
+    is Adj {
+        H(qs[0]);
+        CNOT(qs[0], qs[1]);
     }
 
 
     // Task 2.2. Alice's quantum strategy
     operation AliceQuantum_Reference (bit : Bool, qubit : Qubit) : Bool {
-        if (bit) {
-            // Measure in sign basis if bit is 1
-            return BoolFromResult(Measure([PauliX], [qubit]));
-        } else {
-            // Measure in computational basis if bit is 0
-            return BoolFromResult(Measure([PauliZ], [qubit]));
-        }
+        // Measure in sign basis if bit is 1, and
+        // measure in computational basis if bit is 0
+        let basis = bit ? PauliX | PauliZ;
+        return BoolFromResult(Measure([basis], [qubit]));
     }
 
 
     // Task 2.3. Rotate Bob's qubit
     operation RotateBobQubit_Reference (clockwise : Bool, qubit : Qubit) : Unit {
-        if (clockwise) {
-            Ry(-2.0 * PI() / 8.0, qubit);
-        } else {
-            Ry(2.0 * PI() / 8.0, qubit);
-        }
+        let angle = 2.0 * PI() / 8.0;
+        Ry(clockwise ? -angle | angle, qubit);
     }
 
 
@@ -85,20 +76,18 @@ namespace Quantum.Kata.CHSHGame {
     // Task 2.5. Play the CHSH game
     operation PlayQuantumCHSH_Reference (askAlice : (Qubit => Bool),
                                          askBob : (Qubit => Bool)) : (Bool, Bool) {
-        mutable aliceResult = false;
-        mutable bobResult = false;
 
         using ((aliceQubit, bobQubit) = (Qubit(), Qubit())) {
             CreateEntangledPair_Reference([aliceQubit, bobQubit]);
 
-            set aliceResult = askAlice(aliceQubit);
-            set bobResult = askBob(bobQubit);
+            let aliceResult = askAlice(aliceQubit);
+            let bobResult = askBob(bobQubit);
 
             Reset(aliceQubit);
             Reset(bobQubit);
+            return (aliceResult, bobResult);
         }
 
-        return (aliceResult, bobResult);
     }
 
 }

commit ccd488a68c0adc215808e8c292dc5f2019b5ce7a

Bug fix for phase estimation kata (#113)
This fixed a bug in the logic condition in task 2.2 introduced in https://github.com/microsoft/QuantumKatas/pull/111.

PhaseEstimation/ReferenceImplementation.qs
@@ -135,7 +135,7 @@ namespace Quantum.Kata.PhaseEstimation {
                 H(control);
 
                 let meas = MResetZ(control);
-                set (measuredZero, measuredOne) = (meas == Zero, meas == One);
+                set (measuredZero, measuredOne) = (measuredZero or meas == Zero, measuredOne or meas == One);
             } 
             // repeat the loop until we get both Zero and One measurement outcomes
             // or until we're reasonably certain that we won't get a different outcome