d470f9a78303d90ebc9c619cfadb363b16e66583
ms-quantum build agent <qplat-alerts@microsoft.com>
2019-12-12 09:13:00 +0000

    Build 0.10.1912.1206 changes. [skip ci]

--- scripts/install-iqsharp.ps1
+++ scripts/install-iqsharp.ps1
@@ -7,7 +7,7 @@ $ErrorActionPreference = 'Stop'
 # Install iqsharp if not installed yet.
 dotnet iqsharp --version
 If ($LastExitCode -ne 0) {
-    dotnet tool install Microsoft.Quantum.IQSharp --version 0.10.1912.1102-beta --tool-path $Env:TOOLS_DIR
+    dotnet tool install Microsoft.Quantum.IQSharp --version 0.10.1912.1206-beta --tool-path $Env:TOOLS_DIR
 
     $path = (Get-Item "$Env:TOOLS_DIR\dotnet-iqsharp*").FullName
     Write-Host "iq# installed at $path"
