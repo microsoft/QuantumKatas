4990b4ed4bf9bfb9706dc33b08e12db92badb8e9
Andres Paz <anpaz@microsoft.com>
2020-03-02 08:53:04 -0800

    install iq# resilience

--- scripts/install-iqsharp.ps1
+++ scripts/install-iqsharp.ps1
@@ -5,12 +5,27 @@ $ErrorActionPreference = 'Stop'
 & "$PSScriptRoot/set-env.ps1"
 
 # Install iqsharp if not installed yet.
-dotnet iqsharp --version
-If ($LastExitCode -ne 0) {
+
+Write-Host ("Installing IQ# tool.")
+$install = $False
+
+# Install iqsharp if not installed yet.
+try {
+    dotnet iqsharp --version
+    $install = ($LastExitCode -ne 0)
+} catch {
+    Write-Host ("`dotnet iqsharp --version` threw Exception.")
+    $install = $True
+}
+
+if ($install) {
+    Write-Host ("Installing Microsoft.Quantum.IQSharp at $Env:TOOLS_DIR")
     dotnet tool install Microsoft.Quantum.IQSharp --version 0.10.1911.1607 --tool-path $Env:TOOLS_DIR
 
     $path = (Get-Item "$Env:TOOLS_DIR\dotnet-iqsharp*").FullName
     Write-Host "iq# installed at $path"
     & $path install --user --path-to-tool $path
+} else {
+    Write-Host ("Microsoft.Quantum.IQSharp is already installed in this host.")
 }
 
