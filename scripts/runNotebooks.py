31ae5879efc07ebf243b85c0aae6df002e753d35
ANIRUDHA KULKARNI <kulkarnianirudha8@gmail.com>
2020-07-24 12:21:20 +0530

    Delete runNotebooks.py

--- scripts/runNotebooks.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-import os
-os.system("cd .. & jupyter notebook index.ipynb")
-input()	#This input line stops immediate closing of terminal
