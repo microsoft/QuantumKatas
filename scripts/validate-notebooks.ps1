093e51362ddbe82b98fc1e847ee82fcca85062bf
Vincent van Wingerden <25651976+vivanwin@users.noreply.github.com>
2020-09-08 23:20:24 +0200

    Update validate-notebooks.ps1

--- scripts/validate-notebooks.ps1
+++ scripts/validate-notebooks.ps1
@@ -66,7 +66,8 @@ function Validate {
         if ($version -eq "0.12.20082513") {
             $env:IQSHARP_AUTO_LOAD_PACKAGES = (
                 "Microsoft.Quantum.Standard",
-                "Microsoft.Quantum.Xunit"
+                "Microsoft.Quantum.Xunit",
+                "Microsoft.Quantum.Katas"
             ) -join ","
             Write-Host ("Set IQSHARP_AUTO_LOAD_PACKAGES: " + $env:IQSHARP_AUTO_LOAD_PACKAGES)
         }
