ebc1e50bf28d28796d58168ea2bdd60d1ac9c846
Ryan Shaffer <rmshaffer@gmail.com>
2020-10-05 10:57:16 -0700

    Update version number in comments

--- scripts/Update-QDKVersion.ps1
+++ scripts/Update-QDKVersion.ps1
@@ -18,7 +18,7 @@ param(
         
     .EXAMPLE
     
-        PS> ./Update-QDKVersion.ps1 -Version 0.12.20092803
+        PS> ./Update-QDKVersion.ps1 -Version 0.12.20100301
 #>
 
 $katasRoot = Join-Path $PSScriptRoot "\..\"
