2e5221f39b8fd6e3a3fb0bc2effb69998a08f97c
Manvi-Agrawal <manvimeerut@gmail.com>
2020-10-18 15:30:47 +0530

    Remove the notebooks from not_ready list to check if all the problematic tasks of the notebooks have been excluded or not

--- scripts/validate-notebooks.ps1
+++ scripts/validate-notebooks.ps1
@@ -122,23 +122,12 @@ function Validate {
 
 # List of Notebooks that can't be validated for various reasons:
 #  * Check.ipynb is a validation artifact and not an actual kata notebook.
-#  * CHSH and MagicSquare games require implementing two code cells at once before running the test, 
-#    so the first of the cells implemented is guaranteed to fail.
-#  * GraphColoring and SolveSATWithGrover (and its Workbook) have tasks for which the correct solution fails or times out with relatively high probability.
-#  * ExploringGroversAlgorithm has tasks with deliberately invalid Q# code.
 #  * ComplexArithmetic and LinearAlgebra have tasks with deliberately invalid Python code.
 #  * RandomNumberGenerationTutorial have tasks to generate random numbers but if it is run a lot of times then randomly generated numbers will look insufficiently random
 #
 $not_ready = 
 @(
     'Check.ipynb',
-    'GraphColoring.ipynb',
-    'Workbook_GraphColoring.ipynb',
-    'MagicSquareGame.ipynb',
-    'SolveSATWithGrover.ipynb',
-    'Workbook_SolveSATWithGrover.ipynb',
-    'ExploringGroversAlgorithmTutorial.ipynb',
-    'VisualizingGroversAlgorithm.ipynb',
     'ComplexArithmetic.ipynb',
     'LinearAlgebra.ipynb',
 	'RandomNumberGenerationTutorial.ipynb'
