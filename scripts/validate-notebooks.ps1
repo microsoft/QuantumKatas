205cb155220e05ed1f92be8fdb3c90c2d7fcf741
Manvi-Agrawal <40084144+Manvi-Agrawal@users.noreply.github.com>
2020-10-20 10:53:15 +0530

    Update notebooks validation script to exclude individual tasks (#539)
    This change introduces a mechanism to exclude certain tasks from CI validation.
    See https://github.com/microsoft/QuantumKatas/blob/main/.github/CONTRIBUTING.md#excluding-individual-tasks-from-validation for details.

--- scripts/validate-notebooks.ps1
+++ scripts/validate-notebooks.ps1
@@ -83,20 +83,24 @@ function Validate {
             </configuration>
         " | Out-File ./NuGet.Config -Encoding utf8
 
+        # See the explanation for excluding individual tasks in Contribution guide at 
+        # https://github.com/microsoft/QuantumKatas/blob/main/.github/CONTRIBUTING.md#excluding-individual-tasks-from-validation
+        $exclude_from_validation = "['multicell_solution', 'randomized_solution', 'timeout', 'invalid_code', 'work_in_progress']"
+
         # Run Jupyter nbconvert to execute the kata.
         # dotnet-iqsharp writes some output to stderr, which causes PowerShell to throw
         # unless $ErrorActionPreference is set to 'Continue'.
         $ErrorActionPreference = 'Continue'
         if ($env:SYSTEM_DEBUG -eq "true") {
             # Redirect stderr output to stdout to prevent an exception being incorrectly thrown.
-            jupyter nbconvert $CheckNotebook --execute --to html --ExecutePreprocessor.timeout=300 --log-level=DEBUG 2>&1 | %{ "$_"}
+            jupyter nbconvert $CheckNotebook --TagRemovePreprocessor.remove_cell_tags=$exclude_from_validation  --execute --to html --ExecutePreprocessor.timeout=300 --log-level=DEBUG 2>&1 | %{ "$_"}
         } else {
             # Redirect stderr output to stdout to prevent an exception being incorrectly thrown.
-            jupyter nbconvert $CheckNotebook --execute --to html --ExecutePreprocessor.timeout=300 2>&1 | %{ "$_"}
+            jupyter nbconvert $CheckNotebook --TagRemovePreprocessor.remove_cell_tags=$exclude_from_validation --execute --to html --ExecutePreprocessor.timeout=300 2>&1 | %{ "$_"}
         }
         $ErrorActionPreference = 'Stop'
 
-        # if jupyter returns an error code, report that this notebook is invalid:
+        # if Jupyter returns an error code, report that this notebook is invalid:
         if ($LastExitCode -ne 0) {
             Write-Host "##vso[task.logissue type=error;]Validation errors for $Notebook ."        
             $script:all_ok = $false
@@ -114,26 +118,14 @@ function Validate {
 
 # List of Notebooks that can't be validated for various reasons:
 #  * Check.ipynb is a validation artifact and not an actual kata notebook.
-#  * CHSH and MagicSquare games require implementing two code cells at once before running the test, 
-#    so the first of the cells implemented is guaranteed to fail.
-#  * GraphColoring and SolveSATWithGrover (and its Workbook) have tasks for which the correct solution fails or times out with relatively high probability.
-#  * ExploringGroversAlgorithm has tasks with deliberately invalid Q# code.
 #  * ComplexArithmetic and LinearAlgebra have tasks with deliberately invalid Python code.
-# 
+#  * RandomNumberGenerationTutorial have tasks to generate random numbers but sometimes these numbers will look insufficiently random and fail validation.
 $not_ready = 
 @(
     'Check.ipynb',
-    'CHSHGame.ipynb',
-    'Workbook_CHSHGame.ipynb',
-    'GraphColoring.ipynb',
-    'Workbook_GraphColoring.ipynb',
-    'MagicSquareGame.ipynb',
-    'SolveSATWithGrover.ipynb',
-    'Workbook_SolveSATWithGrover.ipynb',
-    'ExploringGroversAlgorithmTutorial.ipynb',
-    'VisualizingGroversAlgorithm.ipynb',
     'ComplexArithmetic.ipynb',
-    'LinearAlgebra.ipynb'
+    'LinearAlgebra.ipynb',
+    'RandomNumberGenerationTutorial.ipynb'
 )
 
 
