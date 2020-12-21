c33354a1c512d746c77c2952cd67b537522b7d2d
Vincent van Wingerden <25651976+vivanwin@users.noreply.github.com>
2020-12-21 16:27:42 +0100

    CI CI

--- scripts/validate-notebooks.ps1
+++ scripts/validate-notebooks.ps1
@@ -33,7 +33,7 @@ Param(
     [int]$EndIndex = -1.0
 )
 
-if($env:IQSharp.Hosting.Env -eq 'build-agent-katas'){
+if($(IQSharp.Hosting.Env) -eq "build-agent-katas"){
     & "$PSScriptRoot/install-iqsharp.ps1"
 } else {
     Write-Host "We are running localy, so not installing IQSharp"
