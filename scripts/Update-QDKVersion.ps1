7834d60738fc39a30835f17f37b5253a85ee046e
Robin Kuzmin <kuzmin.robin@gmail.com>
2021-09-01 15:10:53 -0700

    Updated the QDK version

--- scripts/Update-QDKVersion.ps1
+++ scripts/Update-QDKVersion.ps1
@@ -18,7 +18,7 @@ param(
         
     .EXAMPLE
     
-        PS> ./Update-QDKVersion.ps1 -Version 0.18.2107153439
+        PS> ./Update-QDKVersion.ps1 -Version 0.18.2108160999
 #>
 
 $katasRoot = Join-Path $PSScriptRoot "\..\"
