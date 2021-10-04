55a25edcfef5ff1213225d7509aa4d46d5ebb33a
Deepto <ddepp98@outlook.com>
2021-10-04 21:39:29 +0530

    Updated prebuild-kata script to exclude projects without .csproj files

--- scripts/prebuild-kata.sh
+++ scripts/prebuild-kata.sh
@@ -4,5 +4,10 @@ KATA_NOTEBOOK=${2:-$1.ipynb}
 
 echo "Prebuilding: $KATA_NOTEBOOK in $KATA_FOLDER kata..."
 
+count=`ls -1 *.csproj 2>/dev/null | wc -l`
+if [ $count != 0 ]
+then 
 dotnet build $KATA_FOLDER
+fi
+
 jupyter nbconvert $KATA_FOLDER/$KATA_NOTEBOOK --execute --to markdown  --allow-errors  --ExecutePreprocessor.timeout=120
