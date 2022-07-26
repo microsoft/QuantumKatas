58a41fa813cee7e9b5181cca901b037671f60c5c
Ricardo Espinoza <ricardoe@microsoft.com>
2022-07-26 12:28:57 -0700

    Update Katas to version 0.25.222597

--- scripts/Update-QDKVersion.ps1
+++ scripts/Update-QDKVersion.ps1
@@ -18,7 +18,7 @@ param(
         
     .EXAMPLE
     
-        PS> ./Update-QDKVersion.ps1 -Version 0.25.218240
+        PS> ./Update-QDKVersion.ps1 -Version 0.25.222597
 #>
 
 $katasRoot = Join-Path $PSScriptRoot "\..\"
