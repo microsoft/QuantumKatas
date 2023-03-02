b856585f329991a03a1df51e909c626e4d4321d4
Scott Carda <55811729+ScottCarda-MS@users.noreply.github.com>
2023-03-01 17:11:58 -0800

    Update to QDK version 0.27.258160 (#876)

--- scripts/Update-QDKVersion.ps1
+++ scripts/Update-QDKVersion.ps1
@@ -18,7 +18,7 @@ param(
         
     .EXAMPLE
     
-        PS> ./Update-QDKVersion.ps1 -Version 0.27.253010
+        PS> ./Update-QDKVersion.ps1 -Version 0.27.258160
 #>
 
 $katasRoot = Join-Path $PSScriptRoot "\..\"
