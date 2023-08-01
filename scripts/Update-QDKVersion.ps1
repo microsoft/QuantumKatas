42c30731374fd4fea0c87baac67542d9980ec905
Robin Kuzmin <kuzmin.robin@gmail.com>
2023-08-01 12:44:09 -0700

    0.28.291394

--- scripts/Update-QDKVersion.ps1
+++ scripts/Update-QDKVersion.ps1
@@ -18,7 +18,7 @@ param(
         
     .EXAMPLE
     
-        PS> ./Update-QDKVersion.ps1 -Version 0.28.277227
+        PS> ./Update-QDKVersion.ps1 -Version 0.28.291394
 #>
 
 $katasRoot = Join-Path $PSScriptRoot "\..\"
